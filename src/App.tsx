3cd1cb2b2294bcc780b72cd1d72a1d4c29cfb164
Thanthai Jitprathum <thanthai.j@odds.team>
2023-07-11 01:21:00 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section>
+      <div className="container">
+        <div className="left"></div>
+        <div className="right">
+          <div className="content">
+            <h1>This is my profile</h1>
+            <h3>Hi, I'm Than</h3>
+            <p>I'm a Frontend Developer</p>
+            <div className="social">
+              <a href="https://www.facebook.com/tanthai.jitprathum" className="fa fa-facebook"></a>
+              <a href="https://www.linkedin.com/in/thanthai-jitprathum/" className="fa fa-linkedin"></a>
+              <a href="https://github.com/Thanthai050345" className="fa fa-github"></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default App;
